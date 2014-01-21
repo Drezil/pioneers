1490b25f1fabb6a7197e7d1277736f852f039f85
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2014-01-21 01:08:17 +0100

    reentered the code for installing libsdl2-2.0.1 as we use functions of it

--- deps/getDeps.sh
+++ deps/getDeps.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#hack until saucy has 2.0.1 instead of 2.0.0
+
+dialog --yesno "Install libSDL2.0.1 from ubuntu trusty?\nCurrently needed for saucy as they only serve 2.0.0 in the repos" 10 50
+install=${?}
+
+if [[ $install -eq 0 ]]
+then
+	echo "installing libsdl2.0.1"
+	if [ ! -f "libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb" ]
+	then
+		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2/libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb
+		sudo dpkg -i libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb
+	fi
+	if [ ! -f "libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb" ]
+	then
+		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2/libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb
+		sudo dpkg -i libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb
+	fi
+fi
+
+## hack end
+
 echo "cloning repositories"
 if [ ! -d "hsSDL2" ]
 then
@@ -19,6 +41,7 @@ do
 	else
 		echo "building: $d ..."
 		cd "$d"
+		cabal clean
 		cabal configure
 		cabal build
 		cabal haddock --hyperlink-source
@@ -26,24 +49,4 @@ do
 		cd ..
 	fi
 done
-
-#hack until saucy has 2.0.1 instead of 2.0.0
-
-#dialog --yesno "Install libSDL2.0.1 from ubuntu trusty?\nCurrently needed for saucy as they only serve 2.0.0 in the repos" 10 50
-#install=${?}
-#
-#if [[ $install -eq 0 ]]
-#then
-#	echo "installing libsdl2.0.1"
-#	if [ ! -f "libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb" ]
-#	then
-#		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2/libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb
-#		sudo dpkg -i libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb
-#	fi
-#	if [ ! -f "libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb" ]
-#	then
-#		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2/libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb
-#		sudo dpkg -i libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb
-#	fi
-#fi
 
