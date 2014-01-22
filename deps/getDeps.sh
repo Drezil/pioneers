a272e583bb903ebf3816fedfd26177943c90bdcc
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2014-01-22 15:28:29 +0100

    travis-changes

--- deps/getDeps.sh
+++ deps/getDeps.sh
@@ -2,10 +2,14 @@
 
 #hack until saucy has 2.0.1 instead of 2.0.0
 
-sudo apt-get install dialog
-
-dialog --yesno "Install libSDL2.0.1 from ubuntu trusty?\nCurrently needed for saucy as they only serve 2.0.0 in the repos\n\nThe script will try to download the trusty-packages and resolve dependencies via gdebi" 20 75
-install=${?}
+if [ "$1" != "ni" ]
+then
+	sudo apt-get install dialog
+	dialog --yesno "Install libSDL2.0.1 from ubuntu trusty?\nCurrently needed for saucy as they only serve 2.0.0 in the repos\n\nThe script will try to download the trusty-packages and resolve dependencies via gdebi" 20 75
+	install=${?}
+else
+	install=0
+fi
 
 if [[ $install -eq 0 ]]
 then
@@ -14,37 +18,37 @@ then
 	if [ ! -f "libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb" ]
 	then
 		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2/libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb
-		sudo gdebi libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb
+		sudo gdebi --n libsdl2-2.0-0_2.0.1+dfsg1-1ubuntu1_amd64.deb
 	fi
 	if [ ! -f "libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb" ]
 	then
 		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2/libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb
-		sudo gdebi libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb
+		sudo gdebi --n libsdl2-dev_2.0.1+dfsg1-1ubuntu1_amd64.deb
 	fi
 	if [ ! -f "libsdl2-dbg_2.0.1+dfsg1-1ubuntu1_amd64.deb" ]
 	then
 		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2/libsdl2-dbg_2.0.1+dfsg1-1ubuntu1_amd64.deb
-		sudo gdebi libsdl2-dbg_2.0.1+dfsg1-1ubuntu1_amd64.deb
+		sudo gdebi --n libsdl2-dbg_2.0.1+dfsg1-1ubuntu1_amd64.deb
 	fi
 	if [ ! -f "libsdl2-ttf-2.0-0_2.0.12+dfsg1-2_amd64.deb" ]
 	then
 		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2-ttf/libsdl2-ttf-2.0-0_2.0.12+dfsg1-2_amd64.deb
-		sudo gdebi libsdl2-ttf-2.0-0_2.0.12+dfsg1-2_amd64.deb
+		sudo gdebi --n libsdl2-ttf-2.0-0_2.0.12+dfsg1-2_amd64.deb
 	fi
 	if [ ! -f "libsdl2-ttf-dev_2.0.12+dfsg1-2_amd64.deb" ]
 	then
 		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2-ttf/libsdl2-ttf-dev_2.0.12+dfsg1-2_amd64.deb
-		sudo gdebi libsdl2-ttf-dev_2.0.12+dfsg1-2_amd64.deb
+		sudo gdebi --n libsdl2-ttf-dev_2.0.12+dfsg1-2_amd64.deb
 	fi
 	if [ ! -f "libsdl2-image-2.0-0_2.0.0+dfsg-3_amd64.deb" ]
 	then
 		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2-image/libsdl2-image-2.0-0_2.0.0+dfsg-3_amd64.deb
-		sudo gdebi libsdl2-image-2.0-0_2.0.0+dfsg-3_amd64.deb
+		sudo gdebi --n libsdl2-image-2.0-0_2.0.0+dfsg-3_amd64.deb
 	fi
 	if [ ! -f "libsdl2-image-dev_2.0.0+dfsg-3_amd64.deb" ]
 	then
 		wget http://de.archive.ubuntu.com/ubuntu/pool/universe/libs/libsdl2-image/libsdl2-image-dev_2.0.0+dfsg-3_amd64.deb
-		sudo gdebi libsdl2-image-dev_2.0.0+dfsg-3_amd64.deb
+		sudo gdebi --n libsdl2-image-dev_2.0.0+dfsg-3_amd64.deb
 	fi
 fi
 ## hack end
