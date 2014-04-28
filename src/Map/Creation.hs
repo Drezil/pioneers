683b72a413b356d99635d31ddc6ed0fae9e22955
Jonas Betzendahl <jbetzend@techfak.uni-bielefeld.de>
2014-04-29 01:05:05 +0200

    Refactoring & more cosmetics

--- src/Map/Creation.hs
+++ src/Map/Creation.hs
@@ -2,9 +2,25 @@ module Map.Creation
 where
 
 import Map.Types
+import Map.StaticMaps
 -- import Map.Map unused (for now)
 
 import Data.Array
+import System.Random
+
+-- preliminary
+infix 5 ->-
+(->-) :: (PlayMap -> PlayMap) -> (PlayMap -> PlayMap) -> PlayMap -> PlayMap
+f ->- g = g . f
+
+-- also preliminary
+infix 5 -<-
+(-<-) :: (PlayMap -> PlayMap) -> (PlayMap -> PlayMap) -> PlayMap -> PlayMap
+f -<- g = f . g
+
+exportedMap :: IO PlayMap
+exportedMap = do mounts <- mnt
+                 return $ aplAll mounts mapEmpty
 
 -- | Generate a new Map of given Type and Size
 --
@@ -22,6 +38,9 @@ aplByNode f g mp = array (bounds mp) (map (\(ab,c) -> (if g c then (ab, f c) els
 
 aplAll :: [a -> a] -> a -> a
 aplAll fs m = foldl (\ n f -> f n) m fs
+
+aplAllM :: Monad m => [m a -> m a] -> m a -> m a
+aplAllM fs x = foldl (\ n f -> f n) x fs
 
 -- general 3D-Gaussian
 gauss3Dgeneral :: Floating q =>
@@ -58,3 +77,37 @@ heightToTerrain GrassIslandMap y
                 | y < 10    = Hill
                 | otherwise = Mountain
 heightToTerrain _ _ = undefined
+
+
+lake :: Int -> PlayMap -> PlayMap
+lake = undefined
+
+river :: Int -> PlayMap -> PlayMap
+river = undefined
+
+mnt :: IO [PlayMap -> PlayMap]
+mnt = do g <- newStdGen
+         let seeds = take 10 $ randoms g
+         return $ map (gaussMountain) seeds
+
+gaussMountain :: Int -> PlayMap -> PlayMap
+gaussMountain seed mp = aplByPlace (liftUp c) (\(_,_) -> True) mp
+  where
+    g   = mkStdGen seed
+    c   = let ((a,b), (x,y)) = bounds mp in (head (randomRs (a,x) g), (head (randomRs (b,y) g)))
+    amp = head $ randomRs (5.0, 20.0) g
+    sig = head $ randomRs (5.0, 25.0) g
+    fi  = fromIntegral
+    htt = heightToTerrain
+
+    -- TODO: Fix Lambda to True with sensible function, maybe rework giveNeighbourhood in Map.Map
+    liftUp :: (Int, Int) -> Node -> Node
+    liftUp (gx,gz) (Full     (x,z) y _ b pl pa r s) = let y_neu = max y e
+                                                      in  Full (x,z) y_neu (htt GrassIslandMap y_neu) b pl pa r s
+      where e = gauss3Dgeneral amp (fi gx) (fi gz) sig sig (fi x) (fi z)
+    liftUp (gx, gz) (Minimal (x,z)) = Full (x,z) e (htt GrassIslandMap e) BFlag NoPlayer NoPath Plain []
+      where e = gauss3Dgeneral amp (fi gx) (fi gz) sig sig (fi x) (fi z)
+
+-- | Makes sure the edges of the Map are mountain-free
+makeIsland :: PlayMap -> PlayMap
+makeIsland = undefined -- tomorrow....
